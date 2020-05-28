9c9f586577c9f16b7c7774e310359afa90e63218
AngelAngelov <angelangelovw1@gmail.com>
2020-05-28 23:37:29 +0300

    3_Activity_Done

---
+++ app/src/main/java/com/example/pustore/CategoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.pustore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_activity);
+    }
+}
